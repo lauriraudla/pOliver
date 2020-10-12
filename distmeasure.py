335fb7e90a299e2e73f70ae5f3ed340f12e94668
lauriraudla <62912145+lauriraudla@users.noreply.github.com>
2020-10-12 18:51:28 +0300

    Add files via upload

---
+++ distmeasure.py
@@ -0,0 +1,18 @@
+import cv2
+import vision_test
+
+
+
+image_thread = vision_test.imageCapRS2()
+
+while True:
+
+    frame = image_thread.getFrame()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    cv2.imshow("frame",frame)
+    k = cv2.waitKey(1)
+    print(image_thread.getDistance(320,320))
+    if k == ord("q"):
+        image_thread.setStopped(False)
+        break
+cv2.destroyAllWindows()
